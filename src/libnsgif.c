db49e5b61beb1759469f3e6bd53850c6bc0a38ac
Michael Drake <tlsa@netsurf-browser.org>
2021-04-04 22:29:34 +0100

    gif: Handle any uncompressed output before exiting due to error.

--- src/libnsgif.c
+++ src/libnsgif.c
@@ -672,8 +672,6 @@ gif__decode(gif_animation *gif,
                         const uint8_t *uncompressed;
                         unsigned row_available;
                         if (available == 0) {
-                                res = lzw_decode(gif->lzw_ctx,
-                                                &uncompressed, &available);
                                 if (res != LZW_OK) {
                                         /* Unexpected end of frame, try to recover */
                                         if (res == LZW_OK_EOD) {
@@ -683,6 +681,8 @@ gif__decode(gif_animation *gif,
                                         }
                                         break;
                                 }
+                                res = lzw_decode(gif->lzw_ctx,
+                                                &uncompressed, &available);
                         }
 
                         row_available = x < available ? x : available;
