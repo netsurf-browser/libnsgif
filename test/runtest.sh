a0b9fcb4fa4c53319726044c8278a551293af817
Michael Drake <tlsa@netsurf-browser.org>
2022-02-26 12:05:51 +0000

    Test: Use tabs for indent in test runner script.
    Previously it was 4 spaces for first level indent, 1 tab for second
    level, 1 tab plus 4 spaces for third level.

--- test/runtest.sh
+++ test/runtest.sh
@@ -1,4 +1,4 @@
-#!/bin/sh 
+#!/bin/sh
 
 # run test images through libnsgif and count results
 
@@ -20,25 +20,25 @@ GIFTESTS="${GIFTESTS} test/ns-afl-gif/*.gif"
 
 gifdecode()
 {
-    OUTF=$(basename ${1} .gif)
-    CMPF=$(dirname  ${1})/${OUTF}.ppm
-    echo "GIF:${1}" >> ${TEST_LOG}
-    ${TEST_PATH}/test_nsgif ${1} ${TEST_OUT}/${OUTF}.ppm 2>> ${TEST_LOG}
-    ECODE=$?
+	OUTF=$(basename ${1} .gif)
+	CMPF=$(dirname  ${1})/${OUTF}.ppm
+	echo "GIF:${1}" >> ${TEST_LOG}
+	${TEST_PATH}/test_nsgif ${1} ${TEST_OUT}/${OUTF}.ppm 2>> ${TEST_LOG}
+	ECODE=$?
 
-    echo "Exit code:${ECODE}" >> ${TEST_LOG}
-    if [ "${ECODE}" -gt 0 ];then
-	return ${ECODE}
-    fi
-
-    if [ -f "${CMPF}" ]; then
-	cmp ${CMPF} ${TEST_OUT}/${OUTF}.ppm >> ${TEST_LOG} 2>> ${TEST_LOG}
-	if [ "$?" -ne 0 ]; then
-	    return 128
+	echo "Exit code:${ECODE}" >> ${TEST_LOG}
+	if [ "${ECODE}" -gt 0 ];then
+		return ${ECODE}
 	fi
-    fi
 
-    return 0
+	if [ -f "${CMPF}" ]; then
+		cmp ${CMPF} ${TEST_OUT}/${OUTF}.ppm >> ${TEST_LOG} 2>> ${TEST_LOG}
+		if [ "$?" -ne 0 ]; then
+			return 128
+		fi
+	fi
+
+	return 0
 }
 
 GIFTESTTOTC=0
@@ -49,27 +49,27 @@ GIFTESTERRC=0
 echo "Testing GIF decode"
 
 for GIF in $(ls ${GIFTESTS});do
-    GIFTESTTOTC=$((GIFTESTTOTC+1))
-    #echo "${GIF}"
-    gifdecode ${GIF}
-    ECODE=$?
-    if [ "${ECODE}" -gt 127 ];then
-	GIFTESTERRC=$((GIFTESTERRC+1))
-	echo "Error ${GIF}"
-    else
-	if [ "${ECODE}" -gt 0 ];then
-	    GIFTESTFAILC=$((GIFTESTFAILC+1))
+	GIFTESTTOTC=$((GIFTESTTOTC+1))
+	#echo "${GIF}"
+	gifdecode ${GIF}
+	ECODE=$?
+	if [ "${ECODE}" -gt 127 ];then
+		GIFTESTERRC=$((GIFTESTERRC+1))
+		echo "Error ${GIF}"
 	else
-	    GIFTESTPASSC=$((GIFTESTPASSC+1))
+		if [ "${ECODE}" -gt 0 ];then
+			GIFTESTFAILC=$((GIFTESTFAILC+1))
+		else
+			GIFTESTPASSC=$((GIFTESTPASSC+1))
+		fi
 	fi
-    fi
 done
 
 echo "Tests:${GIFTESTTOTC} Pass:${GIFTESTPASSC} Fail:${GIFTESTFAILC} Error:${GIFTESTERRC}"
 
 # exit code
 if [ "${GIFTESTERRC}" -gt 0 ]; then
-    exit 1
+	exit 1
 fi
 
 exit 0
